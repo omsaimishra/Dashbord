91bdd41369e0d041a1844d6257b7ce2f87b8ee38
omsaimishra <omsaimishra95568@gmail.com>
2025-06-08 16:16:33 +0530

    COMMIT IT

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/My-Admin/', // 👈 must match the repo name exactly
+  base: './',
   plugins: [react()],
 })
